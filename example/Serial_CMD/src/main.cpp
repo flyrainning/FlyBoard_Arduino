c1f20f90762e1a49fdd187e2b116e87ed209e5d8
flyrainning <flyrainning@gmail.com>
2017-07-29 17:28:40 +0800

    update

--- example/Serial_CMD/src/main.cpp
+++ example/Serial_CMD/src/main.cpp
@@ -11,26 +11,48 @@ FBSerial serial;
 
 void setup() {
 
-
-  serial.begin(115200);
-
-
-//Serial.println(setting._version);
+  //初始化串口
+  serial.bind(115200);
 
 
+  //FBCmd绑定
+  serial.println("start bind");
+  Cmd.bind(serial,setting);
 
- Cmd.bind(serial,setting);
-
- struct Cmd_Map m[]={
+  //添加命令列表，两种方式
+  serial.println("add cmd");
+  struct Cmd_Map m[]={
    {"cmd1",&cmd_class::cmd1},
    {"cmd2",&cmd2,&FBCmd::null},
    {"cmd3",&cmd2,&callback},
- };
- Cmd.add_map(m,sizeof(m));
- Cmd.add_map(Cmd_Map{"cmd4",&cmd_class::cmd1,&callback});
+  };
+  Cmd.add_map(m,sizeof(m));
+
+  Cmd.add_map(Cmd_Map{"cmd4",&cmd_class::cmd1,&callback});
+
+  //可以通过配置对象直接修改CMD配置，控制程序行为
+  // setting.echo=true;//打开回显
+  // setting.quiet=false;//发送响应
+  // setting.save();
+
+  //Debug
+  Cmd.Debug_setting();
+  Cmd.Debug_map();
+
+  delay(2000);
 
 
-delay(1000);
+
+
+
+
+
+
+
+
+
+
+
 // Cmd_Frame cf;
 //
 // cf.id=23;
@@ -65,33 +87,8 @@ delay(1000);
 //    CMDList.push_back(fm);
 //  l.push_back(struct Cmd_Map{"set",&f});
 
-  Serial.println("eoff");
-  //setting.echo=false;
-  //Serial.println(setting.echo);
-
-
-// cd.load(CMDList,setting);
-
-// cd.Debgu_setting();
-// Serial.println("e");
-// Serial.println(setting.echo);
-// setting.echo=true;
-// cd.Debgu_setting();
-
-
-// cd.Debgu_map();
-//   CMDList.push_back(Cmd_Map{"aaset",&f});
-// cd.Debgu_map();
-//
-
-
-
-
-Serial.println("start");
 }
 
 void loop() {
-  Serial.println("loop");
-  delay(500);
-
+  Cmd.loop();
 }
